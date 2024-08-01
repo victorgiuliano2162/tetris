3773112b82689a5786b6054240cb453b94aea83b
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-01 17:02:01 -0300

    tetrimino struct

---
+++ src/tetrimino.rs
@@ -0,0 +1,13 @@
+type Piece = Vec<Vec<u8>>;
+type States = Vec<Piece>;
+
+struct Tetrimino {
+    states: States,
+    x: isize,
+    y: usize,
+    current_state: u8,
+}
+
+trait TetriminoGenerator {
+    fn new() -> Tetrimino;
+}
