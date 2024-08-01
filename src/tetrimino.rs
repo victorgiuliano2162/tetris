775e08b1465a039e56e1c5eb571efe53eb28f1cb
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-01 17:12:05 -0300

    created tetrimino snake

--- src/tetrimino.rs
+++ src/tetrimino.rs
@@ -11,3 +11,29 @@ struct Tetrimino {
 trait TetriminoGenerator {
     fn new() -> Tetrimino;
 }
+
+struct TetriminoI;
+
+impl TetriminoGenerator for TetriminoI {
+    fn new() -> Tetrimino {
+        Tetrimino {
+            states: vec![
+                vec![
+                    vec![1, 1, 1, 1],
+                    vec![0, 0, 0, 0],
+                    vec![0, 0, 0, 0],
+                    vec![0, 0, 0, 0],
+                ],
+                vec![
+                    vec![0, 1, 0, 0],
+                    vec![0, 1, 0, 0],
+                    vec![0, 1, 0, 0],
+                    vec![0, 1, 0, 0],
+                ],
+            ],
+            x: 4,
+            y: 0,
+            current_state: 0,
+        }
+    }
+}
