f6b90fcd1281653442e07cc347d8854eb4762461
Victor Giuliano <vitor.giuliano@gmail.com>
2024-08-13 11:40:17 -0300

    constructor method

--- src/tetris.rs
+++ src/tetris.rs
@@ -1,4 +1,5 @@
 use crate::Tetrimino;
+extern crate rand;
 
 struct Tetris {
     game_map: Vec<Vec<u8>>,
@@ -6,4 +7,22 @@ struct Tetris {
     score: u32,
     nb_lines: u32,
     current_piece: Option<Tetrimino>,
+}
+
+impl Tetris {
+    pub fn new() -> Tetris {
+        let mut game_map = Vec::new();
+        for _ in 0..16 {
+            game_map.push(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+        Tetris {
+            game_map: game_map,
+            current_level: 1,
+            score: 0,
+            nb_lines: 0,
+            current_piece: None,
+        }
+    }
+
+    pub fn
 }
