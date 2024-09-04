2eaa77838ba9b243d5755b4eda537a1b76ce63bb
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-04 20:19:22 -0300

    create function make permanent

--- src/tetris.rs
+++ src/tetris.rs
@@ -1,4 +1,7 @@
-use crate::{Tetrimino, TetriminoGenerator, TetriminoI, TetriminoJ, TetriminoL, TetriminoO, TetriminoS, TetriminoT, TetriminoZ};
+use crate::{
+    Tetrimino, TetriminoGenerator, TetriminoI, TetriminoJ, TetriminoL, TetriminoO, TetriminoS,
+    TetriminoT, TetriminoZ,
+};
 extern crate rand;
 
 struct Tetris {
@@ -32,7 +35,9 @@ impl Tetris {
             rand_nb = rand::random::<u8>() % 7;
         }
 
-        unsafe { PREV == rand_nb; }
+        unsafe {
+            PREV == rand_nb;
+        }
 
         match rand_nb {
             0 => TetriminoI::new(),
@@ -47,7 +52,7 @@ impl Tetris {
     }
 
     fn check_lines(&mut self) {
-        let mut y =0;
+        let mut y = 0;
 
         while y < self.game_map.len() {
             let mut complete = true;
@@ -62,12 +67,35 @@ impl Tetris {
             if complete == true {
                 self.game_map.remove(y);
                 y -= 1;
-
             }
             y += 1;
         }
         while self.game_map.len() < 16 {
             self.game_map.insert(0, vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         }
+    }
+
+    fn make_permanent(&mut self) {
+        if let Some(ref mut piece) = self.current_piece {
+            let mut shift_y = 0;
+
+            while shift_y < piece.states[piece.current_state as usize].len()
+                && piece.y + shift_y < self.game_map.len()
+            {
+                let mut shift_x = 0;
+
+                while shift_x < piece.states[piece.current_state as usize][shift_y].len() && (piece.x + shift_x as isize) < self.game_map[piece.y + shift_y].len() as isize {
+                    if piece.states[piece.current_state as usize][shift_y][shift_x] != 0 {
+                        let x = piece.x + shift_x as isize;
+
+                        self.game_map[piece.y + shift_y][x as usize] = piece.states[piece.current_state as usize][shift_y][shift_x];
+                    }
+                    shift_x += 1;
+                }
+                shift_y += 1;
+            }
+        }
+        self.check_lines();
+        self.current_piece = None;
     }
 }
